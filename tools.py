7996d96ce874db43a352759f3a09f229747be403
Waleed Hassan <vkdeku20@gmail.com>
2026-05-01 22:25:50 +0500

    Add plus and subtract functions with decorators

--- tools.py
+++ tools.py
@@ -1,1 +1,25 @@
 
+from agents import function_tool
+
+@function_tool
+def plus (n1:int , n2:int)-> str:
+     """ This is plus function
+     arg:
+     n1:int
+     n2:int
+     return str
+     """
+     print("plus tool is running")
+     return f"your answer is {n1+n2}"
+    
+@function_tool
+async def subtract (n1:int , n2:int)-> str:
+     """ This is plus subtract
+     arg:
+     n1:int
+     n2:int
+     return str
+     """
+     print("subtract tool is running")
+     return f"your answer is {n1-n2}"
+    
